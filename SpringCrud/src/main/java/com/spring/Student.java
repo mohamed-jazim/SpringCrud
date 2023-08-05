4b84637578ee89e15ddf3794ede6e47ed2e7174a
mohamed-jazim <ns.jazim@gmail.com>
2023-08-05 20:22:43 +0530

    Initial commit

---
+++ SpringCrud/src/main/java/com/spring/Student.java
@@ -0,0 +1,22 @@
+package com.spring;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "jazim")
+
+public class Student {
+
+    @Id
+    private int id;
+    @Column(name = "marks")
+    private int mark;
+
+    @Column
+    private String name;
+}
